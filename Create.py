e6b007f7251a4f6055241fe0f71279097cf46c63
RiderxL <55648819+RiderxL@users.noreply.github.com>
2019-09-22 15:10:13 +0800

    Update Create.py

--- Create.py
+++ Create.py
@@ -1,7 +1,7 @@
 import os
 from docx import Document
 
-path = r'C:\\Users\\lemue\\Documents\\School Files\\'
+path = r'C:\\Users\\Username\\Folder where you would save your document\\'
 
 docx_name = input('Create ')
 document = Document()
