9eef4c55abef9790aeb67b8161fc190e4a9b0268
RiderxL <55648819+RiderxL@users.noreply.github.com>
2019-10-09 00:09:56 +0800

    Update Create.py

--- Create.py
+++ Create.py
@@ -1,13 +1,9 @@
 import os
 from docx import Document
 
-path = r'C:\\Users\\Username\\Folder where you would save your document\\'
+path = r'C:\\Users\\lemue_000\Documents\\School Projects\\'
 
-docx_name = input('Create ')
-document = Document()
-docx_path = path + (docx_name + '.docx')
-document.save(docx_path) 
-print("Document successfully made and saved")
-os.startfile(docx_path)
-
-
+docx_name = input('Create File: ')  #Input of the user
+document = Document(path + (docx_name + '.docx')).save()  #Makes the word file and saves it
+print("Document successfuly made and saved.")
+os.startfile(docx_path)  #Initializes the file 
